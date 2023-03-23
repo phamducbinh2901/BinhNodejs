3db94ffc0d27dfbb176e652204a487d31727a74b
Dat <nguyenvom98@gmail.com>
2023-03-23 13:41:04 +0700

    v

---
+++ example.js
@@ -0,0 +1,48 @@
+let batches = [
+    {
+      expirationDate : "2022-01-30T17:00:00.000Z",
+      array:[ 
+        {
+          value: 12
+        },
+        {
+          value: 36
+        },
+        {
+          exchange: "VD-12"
+        },
+      ],
+      key :1
+    
+    },
+    {
+      expirationDate : '2021-03-22T09:29:46.436Z',
+      key:2,
+      array:[ 
+        {
+          value: 114
+        },
+        {
+          value: 255
+        },
+        {
+          exchange: "DE-12"
+        },
+      ],
+    },  
+    {
+      expirationDate : "2022-03-30T17:00:00.000Z",
+      key:3,
+      array:[ 
+        {
+          value: 12
+        },
+        {
+          value: 66
+        },
+        {
+          exchange: "DR-12"
+        },
+      ],
+    },
+  ]
