c0f10bac1c13e1c3e45fb6cd6350cba21378d62d
Dat <nguyenvom98@gmail.com>
2023-03-24 10:45:16 +0700

    exampleObject

---
+++ exampleObject.js
@@ -0,0 +1,40 @@
+let data = [
+    { actionCodes :{
+     NA : 4,
+     QC : 6,
+     NA : 4,
+    },date : 
+    "2022-04-14 T 09 "
+ },
+    { actionCodes :{
+     NA : 4,
+     CU : 3,
+     NA : 4,
+    },date : 
+    "2022-04-14 T 09 "
+ },
+    { actionCodes :{
+     NA : 4,
+     QC : 4,
+     CUU : 4,
+    },date : 
+    "2022-04-14 T 09 "
+ },
+    { actionCodes :{
+     NA : 4,
+     CU : 4,
+     QCC : 4,
+    },date : 
+    "2022-04-14 T 09 "
+ },
+ ]
+
+ 
+//result
+//[
+//   { NA: [ 4, 4, 4, 4 ] },
+//   { QC: [ 6, 0, 4, 0 ] },
+//   { CU: [ 0, 3, 0, 4 ] },
+//   { CUU: [ 0, 0, 4, 0 ] },
+//   { QCC: [ 0, 0, 0, 4 ] }
+// 
